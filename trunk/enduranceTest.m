c7020a9bdeb7d2aab39449578d6374268d97a16e
ben <ben@0647a717-06e4-481b-b645-a9ca167d2ed2>
2008-03-11 20:09:48 +0000

    Deleted all the dxavi stuff.  Switched to mmreader because it will allow us to read multiple frames in at a time.  Plan to use a buffer of ~10 frames or so to speed up the autotracking.

--- trunk/enduranceTest.m
+++
@@ -1,7 +0,0 @@
-[filename, pathname] = uigetfile({'*.avi','AVI file'; '*.*','Any file'},'Pick an AVI file');
-for i=1:500;
-	i
-	[avi_hdl, avi_inf] = dxAviOpen([pathname,filename]);
-	pixmap = dxAviReadMex(avi_hdl, 10);
-	dxAviCloseMex(avi_hdl);
-end
