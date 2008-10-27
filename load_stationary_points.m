e3c17bdcaf0e2662f16ba11701af88ce478d3128
Ben Falk <falkben@users.noreply.github.com>
2008-10-27 19:13:55 +0000

    Added a function to load in stationary points from a previously analyzed file.  Haven't fully error checked the function, but it seems to work.
    Still need to add hooks in the GUI.

---
+++ load_stationary_points.m
@@ -0,0 +1,37 @@
+function load_stationary_points
+global D3_GLOBAL
+
+if ispref('d3_path','analyzed_path')
+    [pn] = getpref('d3_path','analyzed_path');
+else
+    pn = './';
+end
+
+cdir = pwd;
+try
+    cd(pn);
+catch
+    disp('Cannot find your analyzed files directory.  Please check your paths.');
+end;
+[filename, pathname] = uigetfile( '*.d3','Load trial');
+cd(cdir);
+if ~isstr(filename)
+    return
+end
+
+load([pathname filename],'-MAT');
+
+if (~strcmp(num2str(whole_trial.spatial_model.name),num2str(D3_GLOBAL.spatial_model.name)) || ~strcmp(num2str([whole_trial.spatial_model.point(:).name]),num2str([(D3_GLOBAL.spatial_model.point(:).name)])))
+    disp('Spatial models not compatible.');
+    return;
+end
+
+for n = 1:length(whole_trial.spatial_model.point)
+    if whole_trial.spatial_model.point(n).stationary == 1
+        for c = 1:2
+            for xy = 1:2
+                D3_GLOBAL.rawdata.point(n).cam(c).coordinate(1:D3_GLOBAL.max_frames,xy) = whole_trial.rawdata.point(n).cam(c).coordinate(1,xy);
+            end
+        end
+    end
+end
