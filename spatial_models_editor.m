0c8134f38688a2372e1732f0fa978711e902cf13
Ben Falk <falkben@users.noreply.github.com>
2008-12-17 21:11:44 +0000

    Spatial models should be updated to have correct stationary field value

--- spatial_models_editor.m
+++ spatial_models_editor.m
@@ -5,19 +5,20 @@ load([path file]);
 % display all the spatial model names
 disp(['Number of spatial models: ', num2str(length(spatial_model))]);
 for y = 1:length(spatial_model)
-    disp(spatial_model(1,y).name);
+    disp([num2str(y) ': ' spatial_model(1,y).name]);
 end
 
 % remove typo field of statonary from all points in spatial model (should
 % be stationary)
-for y = 1:length(spatial_model)
-    if isfield(spatial_model(1,y).point(1,1), 'statonary')
-        spatial_model(1,y).point = rmfield(spatial_model(1,y).point, 'statonary');
-    end
-end
-
-% for x=2:length(spatial_model(1,28).point)
-%    spatial_model(1,28).point(1,x).stationary = 1;
+% for y = 1:length(spatial_model)
+%     if isfield(spatial_model(1,y).point(1,1), 'statonary')
+%         spatial_model(1,y).point = rmfield(spatial_model(1,y).point, 'statonary');
+%     end
 % end
+
+model = 11;
+for x=2:length(spatial_model(1,model).point)
+   spatial_model(1,model).point(1,x).stationary = 1;
+end
 
 save('spatial_models.mat', 'spatial_model');
