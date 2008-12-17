f94653e0920924cf85d11b3ae12ad7c3c7fc6b3b
ben <ben@0647a717-06e4-481b-b645-a9ca167d2ed2>
2008-12-17 20:57:47 +0000

    Added some code that can be modified when making changes to the spatial models. Updated the spatial models to remove the typo 'statonary' field.  Also modified some of the points so that stationary points are actually stationary in the model.

---
+++ trunk/spatial_models_editor.m
@@ -0,0 +1,23 @@
+%load spatial model
+[file, path] = uigetfile('*.mat','Load a spatial model');
+load([path file]);
+
+% display all the spatial model names
+disp(['Number of spatial models: ', num2str(length(spatial_model))]);
+for y = 1:length(spatial_model)
+    disp(spatial_model(1,y).name);
+end
+
+% remove typo field of statonary from all points in spatial model (should
+% be stationary)
+for y = 1:length(spatial_model)
+    if isfield(spatial_model(1,y).point(1,1), 'statonary')
+        spatial_model(1,y).point = rmfield(spatial_model(1,y).point, 'statonary');
+    end
+end
+
+% for x=2:length(spatial_model(1,28).point)
+%    spatial_model(1,28).point(1,x).stationary = 1;
+% end
+
+save('spatial_models.mat', 'spatial_model');
