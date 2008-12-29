70d379ba75c7d7555b6a9e09732717070f921220
ben <ben@0647a717-06e4-481b-b645-a9ca167d2ed2>
2008-12-29 16:49:04 +0000

    Updated spatial models to remove typo "statonary" point.

--- trunk/spatial_models_editor.m
+++ trunk/spatial_models_editor.m
@@ -10,15 +10,15 @@ end
 
 % remove typo field of statonary from all points in spatial model (should
 % be stationary)
-% for y = 1:length(spatial_model)
-%     if isfield(spatial_model(1,y).point(1,1), 'statonary')
-%         spatial_model(1,y).point = rmfield(spatial_model(1,y).point, 'statonary');
-%     end
-% end
+ for y = 1:length(spatial_model)
+     if isfield(spatial_model(1,y).point(1,1), 'statonary')
+         spatial_model(1,y).point = rmfield(spatial_model(1,y).point, 'statonary');
+     end
+ end
 
-model = 11;
+model = 29;
 for x=2:length(spatial_model(1,model).point)
    spatial_model(1,model).point(1,x).stationary = 1;
 end
 
-save('spatial_models.mat', 'spatial_model');
+% save('spatial_models.mat', 'spatial_model');
