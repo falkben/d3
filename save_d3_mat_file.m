b9b72738660849750ae54a5e4c0b5c07c5a0892d
Ben Falk <falkben@users.noreply.github.com>
2013-06-29 19:10:29 +0000

    Removed try/catch statements in load/save callbacks for better error handling. Removed cd calls for specifying folder to start in for load/save callbacks. Pause for 1/10th of a second before loading file (to allow drive to become ready)? Removed extra calls to update.

--- save_d3_mat_file.m
+++ save_d3_mat_file.m
@@ -1,4 +1,5 @@
-function save_d3_mat_file(fn, D3_GLOBAL)
+function saved = save_d3_mat_file(fn, D3_GLOBAL)
+saved=0;
 
 tcode = D3_GLOBAL.tcode;
 startframe = D3_GLOBAL.d3_analysed.startframe;
@@ -11,11 +12,16 @@ if isfield(D3_GLOBAL,'ignore_segs_cam1') && isfield(D3_GLOBAL,'ignore_segs_cam2'
 end
 
 for n = 1:length(D3_GLOBAL.spatial_model.point)
-    d3_analysed.object(n).name = D3_GLOBAL.spatial_model.point(n).name ;
+  d3_analysed.object(n).name = D3_GLOBAL.spatial_model.point(n).name ;
+  if ~isempty(D3_GLOBAL.reconstructed.point(n).pos)
     d3_analysed.object(n).video = [D3_GLOBAL.reconstructed.point(n).pos(:,1) ...
     -D3_GLOBAL.reconstructed.point(n).pos(:,3) ...
     D3_GLOBAL.reconstructed.point(n).pos(:,2)] ;
+  else
+    return;
+  end
 end
 
 save(fn,'d3_analysed','-V6');
 disp(['Saved ' tcode '_' num2str(startframe) '_d3.mat to: ' fn]);
+saved = 1;
